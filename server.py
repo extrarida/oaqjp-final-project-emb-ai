fe8d8a326e7b048cfb8d8f1089339d2b305515eb
sara rida <sararidapatel@gmail.com>
2025-10-06 08:55:48 -0400

    completed static code check and assignment

--- server.py
+++ server.py
@@ -1,3 +1,7 @@
+"""
+Flask web server for the Emotion Detection application.
+This app receives text input from the user and returns the detected emotion.
+"""
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detector import emotion_detector
 
@@ -5,6 +9,9 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """
+    Function written to display results from package.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
@@ -17,15 +24,18 @@ def sent_detector():
 
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        return f"""For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 
-        'fear': {fear}, 'joy': {joy}, and 'sadness': {sadness}. The dominant emotion is <b>{dominant_emotion}</b>."""
-    
+
+    return f"""For the given statement, the system response is
+    'anger': {anger}, 'disgust': {disgust}, 
+    'fear': {fear}, 'joy': {joy}, and 'sadness': {sadness}. 
+    The dominant emotion is <b>{dominant_emotion}</b>."""
+
 @app.route("/")
 def render_index_page():
+    """
+    To retrieve static html and display with results.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-    
